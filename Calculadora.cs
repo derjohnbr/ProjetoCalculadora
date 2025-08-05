6bf52c8f713c8fe5f29098572c363c3989f54490
DerJohnBR <derjohnbr@gmail.com>
2025-08-05 20:59:22 -0300

    Adiciona a programação do botão Subtrair

---
+++ Calculadora.cs
@@ -0,0 +1,26 @@
+namespace Calculadora
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            double numero1 = Convert.ToDouble(txtNumero1.Text);
+            double numero2 = Convert.ToDouble(txtNumero2.Text);
+
+            lblResultado.Text = (numero1 + numero2).ToString();
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            double numero1 = Convert.ToDouble(txtNumero1.Text);
+            double numero2 = Convert.ToDouble(txtNumero2.Text);
+
+            lblResultado.Text = (numero1 - numero2).ToString();
+        }
+    }
+}
